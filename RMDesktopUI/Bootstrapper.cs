f2dbd8f57adeb032a60631633d3827df7508da24
Sergio Zanetti <sergio.zanetti@outlook.com>
2019-10-02 10:17:40 -0300

    Video 6: Agrego Caliburn.Micro y cambio mi ventana de startup en wpf

---
+++ RMDesktopUI/Bootstrapper.cs
@@ -0,0 +1,30 @@
+﻿using Caliburn.Micro;
+using RMDesktopUI.ViewModels;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+
+namespace RMDesktopUI
+{
+    public class Bootstrapper : BootstrapperBase
+    {
+        public Bootstrapper()
+        {
+            Initialize();
+        }
+
+        protected override void OnStartup(object sender, StartupEventArgs e)
+        {
+            //Cuando arranca este proyecto, selecciona 'ShellViewModel' como Base View
+            //en Appl.xaml dice que la vista default es StartupUri="MainWindow.xaml"
+            //Con esto lo estoy cambiando a ShellViewModel
+            DisplayRootViewFor<ShellViewModel>();
+        }
+
+    }
+
+
+}
