d749ac96649a341be4f185f92b44c84dc3f4ed82
Sergio Zanetti <sergio.zanetti@outlook.com>
2019-10-02 08:45:35 -0300

    Hasta el video 4 con Swagger

---
+++ RMDataManager/App_Start/AuthorizationOperationFilter.cs
@@ -0,0 +1,35 @@
+﻿using Swashbuckle.Swagger;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Http.Description;
+
+namespace RMDataManager.App_Start
+{
+    public class AuthorizationOperationFilter : IOperationFilter
+    {
+        /// <summary>
+        /// Esto agrega un parámetro a cada operación
+        /// </summary>
+        /// <param name="operation"></param>
+        /// <param name="schemaRegistry"></param>
+        /// <param name="apiDescription"></param>
+        public void Apply(Operation operation, SchemaRegistry schemaRegistry, ApiDescription apiDescription)
+        {
+            //Ojo que no es lo mismo que no tenga parámetros a que los parámetros sean null
+            if (operation.parameters == null)
+            {
+                operation.parameters = new List<Parameter>();
+            }
+            operation.parameters.Add(new Parameter
+            {
+                name = "Authorization",
+                @in = "header",
+                description = "access token",
+                required = false,
+                type="string"
+            });
+        }
+    }
+}
