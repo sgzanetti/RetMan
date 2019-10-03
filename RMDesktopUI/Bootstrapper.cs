d45de8a8d01a13fc1b156c20b26b744c82867a51
Sergio Zanetti <sergio.zanetti@outlook.com>
2019-10-03 10:12:02 -0300

    Video 7: Agregado de dependency injection con Caliburn Micro

--- RMDesktopUI/Bootstrapper.cs
+++ RMDesktopUI/Bootstrapper.cs
@@ -11,9 +11,31 @@ namespace RMDesktopUI
 {
     public class Bootstrapper : BootstrapperBase
     {
+        // '_container' Contiene la instanciación de clases para el dependency injection
+        // todos los 'protected override' son para configurar dependency injection
+        private SimpleContainer _container = new SimpleContainer();
         public Bootstrapper()
         {
             Initialize();
+        }
+
+        protected override void Configure()
+        {
+            //Esta es la manera que trabaja Caliber.Micro
+            _container.Instance(_container);
+            _container
+                .Singleton<IWindowManager, WindowManager>()
+                .Singleton<IEventAggregator, EventAggregator>();
+            GetType().Assembly.GetTypes()
+                .Where(type => type.IsClass)
+                .Where(type => type.Name.EndsWith("ViewModel"))
+                .ToList()
+                .ForEach(viewModelType => _container.RegisterPerRequest(viewModelType, viewModelType.ToString(), viewModelType));
+            //Ahora vamos a conectar los ViewModels con las Views
+
+            // Lo que hace esto es: para cada clase en nuestro proyecto que 
+            // termine en 'ViewModel', la registro en mi _container
+            // Hay algún quilombo si queremos hacer nUnit test a muestro proyecto
         }
 
         protected override void OnStartup(object sender, StartupEventArgs e)
@@ -24,7 +46,19 @@ namespace RMDesktopUI
             DisplayRootViewFor<ShellViewModel>();
         }
 
+        protected override object GetInstance(Type service, string key)
+        {
+            return _container.GetInstance(service, key);
+        }
+
+        protected override IEnumerable<object> GetAllInstances(Type service)
+        {
+            return _container.GetAllInstances(service);
+        }
+
+        protected override void BuildUp(object instance)
+        {
+            _container.BuildUp(instance);
+        }
     }
-
-
 }
