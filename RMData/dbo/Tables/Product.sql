1d71d7d0f56f4d1fb15fde655fb7f0bbc4db6b17
Sergio Zanetti <sergio.zanetti@outlook.com>
2019-10-04 09:44:41 -0300

    Video 9-Registro de Tablas

---
+++ RMData/dbo/Tables/Product.sql
@@ -0,0 +1,19 @@
+﻿CREATE TABLE [dbo].[Product]
+(
+	[Id] INT NOT NULL PRIMARY KEY IDENTITY, 
+    [ProductName] NVARCHAR(100) NOT NULL, 
+    [Description] NVARCHAR(MAX) NOT NULL, 
+	[RetailPrice] MONEY NOT NULL,
+    [CreateDate] DATETIME2 NOT NULL DEFAULT getutcdate(), 
+    [LastModified] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
+
+GO
+EXEC sp_addextendedproperty @name = N'MS_Description',
+    @value = N'Veremos si puedo acceder a esto',
+    @level0type = N'SCHEMA',
+    @level0name = N'dbo',
+    @level1type = N'TABLE',
+    @level1name = N'Product',
+    @level2type = NULL,
+    @level2name = NULL
---
+++ RMData/dbo/Tables/User.sql
@@ -0,0 +1,13 @@
+﻿CREATE TABLE [dbo].[User]
+(
+	[Id] INT NOT NULL PRIMARY KEY identity, 
+    [AuthUserId] NVARCHAR(128) NOT NULL, 
+    [FirstName] NCHAR(50) NOT NULL, 
+    [LastName] NCHAR(50) NOT NULL, 
+    [EmailAddress] NCHAR(256) NOT NULL, 
+    [CreationDate] DATETIME2 NOT NULL DEFAULT getutcdate()
+)
+
+GO
+
+CREATE INDEX [IX_User_AuthUserId] ON [dbo].[User] ([AuthUserId])
